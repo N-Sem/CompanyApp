f9a8e2d73054db08ef19bbaed07db9dc964103bf
Sem <nikiforov.semen@gmail.com>
2022-12-09 09:27:43 +0300

    Протестированы различные способы создания команд

---
+++ CompanyApp.UI/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,18 @@
+﻿using CompanyApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CompanyApp.UI.Commands
+{
+    public class CloseApplicationCommand : CommandBase
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) =>
+            Application.Current.Shutdown();
+    }
+}
